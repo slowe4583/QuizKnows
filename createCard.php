3cd7a0670e01a9a3d387bfa427c14e510c8244f3
slowe4583 <50456071+slowe4583@users.noreply.github.com>
2021-10-22 13:41:53 -0400

    Add files via upload

---
+++ createCard.php
@@ -0,0 +1,22 @@
+<html>
+<link rel="stylesheet" href="style.css">
+
+    <h1>Create A Card</h1>
+    <body>
+        Question:<input type="text" id=setQuestion>
+        Answer:<input type="text" id=setAnswer>
+        <br>
+        <br>
+
+        <a href="mySets.php">
+        <button>Done</button>
+        </a>
+       
+        <br>
+        <br>
+
+        <a href="mySets.php">
+        <button>Go Back</button>
+        </a>
+    </body>
+</html>
