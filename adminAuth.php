0e7844242c17549fa3b157d885ba82515cc45388
slowe4583 <50456071+slowe4583@users.noreply.github.com>
2021-10-26 11:26:56 -0400

    Update adminAuth.php

--- adminAuth.php
+++ adminAuth.php
@@ -1,3 +1,14 @@
 <?php
+session_start();
 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    die;
+}
+
+if(!isset($_SESSION["isAdmin"]) || $_SESSION["isAdmin"] < 1) {
+    header("location: index.php");
+    die();
+}
 ?>
