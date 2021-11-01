692c3b80082da9aa4bd1d347351781ab9ce162e3
midnightKristi <69993761+midnightKristi@users.noreply.github.com>
2021-11-01 10:51:01 -0400

    card flip and tracking
    require modification

---
+++ scripts.js
@@ -0,0 +1,121 @@
+$(function() {
+  $("#javascript").click(function () {
+    $("#javascript").hide();
+    $(".javascript").slideToggle();
+  });
+
+  $(".javascript").click(function () {
+    $(".javascript").hide();
+    $("#javascript").fadeIn();
+  });
+
+  $("#operators").click(function () {
+    $("#operators").hide();
+    $(".operators").slideToggle();
+  });
+
+  $(".operators").click(function () {
+    $(".operators").hide();
+    $("#operators").fadeIn();
+  });
+
+  $("#variables").click(function () {
+    $("#variables").hide();
+    $(".variables").slideToggle();
+  });
+
+  $(".variables").click(function () {
+    $(".variables").hide();
+    $("#variables").fadeIn();
+  });
+
+  $("#methods").click(function () {
+    $("#methods").hide();
+    $(".methods").slideToggle();
+  });
+
+  $(".methods").click(function () {
+    $(".methods").hide();
+    $("#methods").fadeIn();
+  });
+
+  $("#arguments").click(function () {
+    $("#arguments").hide();
+    $(".arguments").slideToggle();
+  });
+
+  $(".arguments").click(function () {
+    $(".arguments").hide();
+    $("#arguments").fadeIn();
+  });
+
+  $("#parameters").click(function () {
+    $("#parameters").hide();
+    $(".parameters").slideToggle();
+  });
+
+  $(".parameters").click(function () {
+    $(".parameters").hide();
+    $("#parameters").fadeIn();
+  });
+
+  $("#booleans").click(function () {
+    $("#booleans").hide();
+    $(".booleans").slideToggle();
+  });
+
+  $(".booleans").click(function () {
+    $(".booleans").hide();
+    $("#booleans").fadeIn();
+  });
+
+  $("#functions").click(function () {
+    $("#functions").hide();
+    $(".functions").slideToggle();
+  });
+
+  $(".functions").click(function () {
+    $(".functions").hide();
+    $("#functions").fadeIn();
+  });
+
+  $("#undefined").click(function () {
+    $("#undefined").hide();
+    $(".undefined").slideToggle();
+  });
+
+  $(".undefined").click(function () {
+    $(".undefined").hide();
+    $("#undefined").fadeIn();
+  });
+
+  $("#strings").click(function () {
+    $("#strings").hide();
+    $(".strings").slideToggle();
+  });
+
+  $(".strings").click(function () {
+    $(".strings").hide();
+    $("#strings").fadeIn();
+  });
+
+  $("#nan").click(function () {
+    $("#nan").hide();
+    $(".nan").slideToggle();
+  });
+
+  $(".nan").click(function () {
+    $(".nan").hide();
+    $("#nan").fadeIn();
+  });
+
+  $("#jquery").click(function () {
+    $("#jquery").hide();
+    $(".jquery").slideToggle();
+  });
+
+  $(".jquery").click(function () {
+    $(".jquery").hide();
+    $("#jquery").fadeIn();
+  });
+});
