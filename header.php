a7a0cd14da9f33da622e2895650419897032692b
allikins34 <77242634+allikins34@users.noreply.github.com>
2021-11-05 12:11:43 -0400

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-
- <div class="topNav"div>
- <a class="active" href="../index.php">Home</a>
- <a class="active" href="../search.php">Search</a>
- <a class="active" href="../mySets.php">My Sets</a>
- <a class="active" href="../logOut.php">Logout</a>
-</div>
-
