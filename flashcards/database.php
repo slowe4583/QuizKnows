540805f962b1ec12f9cfb9bc5deadae64afe0ba5
allikins34 <77242634+allikins34@users.noreply.github.com>
2021-11-09 17:46:28 -0500

    Delete database.php

--- flashcards/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Create connection
-$db_conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// Check connection
-if ($db_conn->connect_error) {
-  die("Database connection failed: " . $db_conn->connect_error);
-}
-?>
