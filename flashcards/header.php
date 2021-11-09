9016194b76ab2e7bed29b61307daf375ef484bd6
allikins34 <77242634+allikins34@users.noreply.github.com>
2021-11-09 17:46:14 -0500

    Delete header.php

--- flashcards/header.php
+++
@@ -1,8 +0,0 @@
-
- <div class="topNav"div>
- <a class="active" href="../flashcards/index.php">Home</a>
- <a class="active" href="../search.php">Search</a>
- <a class="active" href="../flashcards/create.php">My Sets</a>
- <a class="active" href="../logOut.php">Logout</a>
-</div>
-
